c40bb0f26d8ca5382542a11539969c132cd88b32
Mat Jordan <mat@northwestern.edu>
2021-09-20 17:41:08 -0400

    Cleanup baseline.

--- src/components/Viewer/Viewer.tsx
+++ src/components/Viewer/Viewer.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { label } from "../../services/IIIF/presentation";
 import { Media } from "../Media/Media";
 import { Navigator } from "../Navigator/Navigator";
 import { Video } from "../Video/Video";
+import { label } from "../../services/IIIF/presentation";
 
 interface Props {
   manifest: object;
@@ -13,7 +13,7 @@ export const Viewer: React.FC<Props> = ({ manifest }) => {
     <>
       <header>{label({ en: ["manifest.label"] }, "en")}</header>
       <div>
-        <Video source="https://video.source..." poster="" tracks={{}} />
+        <Video source="uri" poster="uri" tracks={{}} />
         <Navigator currentTime={0} tracks={{}} />
         <Media items={{}} activeItem={0} />
       </div>
--- src/services/IIIF/presentation.ts
+++ src/services/IIIF/presentation.ts
@@ -2,7 +2,7 @@ type Language = string;
 
 export const label = (
   label: Record<Language, Array<string>>,
-  language: string
+  language: Language
 ) => {
   return label[language][0];
 };
