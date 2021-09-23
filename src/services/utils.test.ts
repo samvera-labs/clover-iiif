cf2665e13ec0d9b98f85181eaa0b708f665a329e
Mat Jordan <mat@northwestern.edu>
2021-09-23 09:19:39 -0400

    Write tests for all digit notations.

--- src/services/utils.test.ts
+++ src/services/utils.test.ts
@@ -1,6 +1,16 @@
 import { cleanTime } from "./utils";
 
 test("Test output a 'cleaned time' when given international standard time notation.", () => {
-  const string = cleanTime("01:22:55.784");
-  expect(string).toBe("1:22:55");
+  const hours1 = cleanTime("11:15:55.784");
+  expect(hours1).toBe("11:15:55");
+  const hours2 = cleanTime("01:15:55.784");
+  expect(hours2).toBe("1:15:55");
+  const minutes1 = cleanTime("00:15:55.784");
+  expect(minutes1).toBe("15:55");
+  const minutes2 = cleanTime("00:05:55.784");
+  expect(minutes2).toBe("5:55");
+  const seconds1 = cleanTime("00:00:55.784");
+  expect(seconds1).toBe("0:55");
+  const seconds2 = cleanTime("00:00:05.784");
+  expect(seconds2).toBe("0:05");
 });
