29c1413fb48b210a35accba73e211fb753ae9ef1
Adam J. Arling <aarling@gmail.com>
2021-09-23 13:51:48 -0500

    Add local linting

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 A React component which renders a YouTube style player and thumbnails UI. We're aiming for:
 
-- Switching between video or audio files
-- Using native HTML `<video />` element
-- Rendering navigable sections based (initially) on `WebVTT` structure
-- Driven by IIIF structure
+-   Switching between video or audio files
+-   Using native HTML `<video />` element
+-   Rendering navigable sections based (initially) on `WebVTT` structure
+-   Driven by IIIF structure
 
-## Development
+## Development environment
+
+This will open up a local dev server with live reloading.
 
 ```
 npm install
@@ -16,6 +18,16 @@ npm run dev
 
 ## Build
 
+This will build and package a component for NPM distribution
+
 ```
 npm run build
+```
+
+### Notes
+
+-   ESBuild compiles TypeScript to JavaScript, but does not do type checking. To view type checking errors (in addtion to what your IDE will be complaining about), run:
+
+```
+tsc
 ```
--- src/components/Media/Media.tsx
+++ src/components/Media/Media.tsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from "react";
-import { styled } from "@stitches/react"
-import MediaItem from "components/MediaItem/MediaItem"
+import React from 'react';
+import { styled } from '@stitches/react';
+import MediaItem from 'components/MediaItem/MediaItem';
 
 interface Props {
-  items: object;
-  activeItem: number;
+    items: object;
+    activeItem: number;
 }
 
 const Media: React.FC<Props> = ({ items, activeItem }) => {
-  return (
-    <MediaWrapper>
-      <MediaItem index={0} label="" thumbnailId="" active={false} />
-      <MediaItem index={1} label="" thumbnailId="" active={true} />
-      <MediaItem index={2} label="" thumbnailId="" active={false} />
-      <MediaItem index={3} label="" thumbnailId="" active={false} />
-      <MediaItem index={4} label="" thumbnailId="" active={false} />
-      <MediaItem index={5} label="" thumbnailId="" active={false} />
-      <MediaItem index={6} label="" thumbnailId="" active={false} />
-      <MediaItem index={7} label="" thumbnailId="" active={false} />
-      <MediaItem index={8} label="" thumbnailId="" active={false} />
-    </MediaWrapper>
-  );
+    return (
+        <MediaWrapper>
+            <MediaItem index={0} label="" thumbnailId="" active={false} />
+            <MediaItem index={1} label="" thumbnailId="" active={true} />
+            <MediaItem index={2} label="" thumbnailId="" active={false} />
+            <MediaItem index={3} label="" thumbnailId="" active={false} />
+            <MediaItem index={4} label="" thumbnailId="" active={false} />
+            <MediaItem index={5} label="" thumbnailId="" active={false} />
+            <MediaItem index={6} label="" thumbnailId="" active={false} />
+            <MediaItem index={7} label="" thumbnailId="" active={false} />
+            <MediaItem index={8} label="" thumbnailId="" active={false} />
+        </MediaWrapper>
+    );
 };
 
-const MediaWrapper = styled("nav", {
-  display: "flex",
-  flexDirection: "row",
-  flexGrow: "1",
-  padding: "1.618rem 0.618rem 1.618rem 0 ",
-  overflowX: "scroll",
-  backgroundColor: "white",
+const MediaWrapper = styled('nav', {
+    display: 'flex',
+    flexDirection: 'row',
+    flexGrow: '1',
+    padding: '1.618rem 0.618rem 1.618rem 0 ',
+    overflowX: 'scroll',
+    backgroundColor: 'white',
 });
 
 export default Media;
