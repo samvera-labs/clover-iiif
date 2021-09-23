4d4890621a74de85d9a9c68deb2cd1546e5c711f
Mat Jordan <mat@northwestern.edu>
2021-09-23 16:30:44 -0400

    Remove unproven validation.

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,23 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import { Vault } from "@hyperion-framework/vault";
-import { Validator } from "@hyperion-framework/validator";
-import { ManifestNormalized } from "@hyperion-framework/types";
-import Viewer from "./components/Viewer/Viewer";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { Vault } from '@hyperion-framework/vault';
+import { ManifestNormalized } from '@hyperion-framework/types';
+import Viewer from './components/Viewer/Viewer';
 
 interface Props {
   id: string;
 }
-
-const validManifest: string = "";
-
-const invalidManifest: string =
-  "https://raw.githubusercontent.com/mathewjordan/mirador-playground/main/assets/iiif/manifest/new_airliner.json";
+const sampleManifest: string =
+  'https://raw.githubusercontent.com/mathewjordan/mirador-playground/main/assets/iiif/manifest/new_airliner_invalid.json';
 
 const App: React.FC<Props> = ({ id }) => {
-  const [manifest, setManifest] = useState<ManifestNormalized | undefined>(
-    undefined
-  );
+  const [manifest, setManifest] = useState<ManifestNormalized | undefined>(undefined);
   const [loaded, setLoaded] = useState(false);
   useEffect(() => {
     const vault = new Vault();
     vault
       .loadManifest(id)
       .then((data) => {
-        const validator = new Validator();
-        if (!validator.validateManifest(data)) {
-          console.log(
-            `Manifest is not valid according IIIF Presentation API 3.0 specification.`
-          );
-        }
         setManifest(data);
         setLoaded(true);
       })
@@ -38,13 +26,13 @@ const App: React.FC<Props> = ({ id }) => {
       });
   }, [loaded]);
 
-  if (typeof manifest !== "undefined") {
+  if (typeof manifest !== 'undefined') {
     return <Viewer manifest={manifest} />;
   }
 
-  return "A future user friendly loading component";
+  return 'A future user friendly loading component';
 };
 
-ReactDOM.render(<App id={invalidManifest} />, document.getElementById("root"));
+ReactDOM.render(<App id={sampleManifest} />, document.getElementById('root'));
 
 export default App;
