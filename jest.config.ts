496fd4b1f6592093a735d51cd57b632c4d635cd0
Adam J. Arling <aarling@gmail.com>
2021-09-23 15:53:57 -0500

    Update Jest config; add @testing-library; sample basic test of Navigator component

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+// jest.config.ts
+import type { Config } from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+  moduleDirectories: ["node_modules", "src"],
+  roots: ["<rootDir>/src"],
+  testEnvironment: "jsdom",
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
+export default config;
