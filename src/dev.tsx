ae15fa9cb50c1bd93243a4a24869413c2d8368a7
Mat Jordan <mat@northwestern.edu>
2021-11-22 09:14:44 -0500

    1.4.2-beta.0

--- src/dev.tsx
+++ src/dev.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./index";
 
 const sampleManifest: string =
-  "http://127.0.0.1:8080/fixtures/iiif/manifests/assortedCanvases.json";
+  "http://127.0.0.1:8080/fixtures/iiif/manifests/canvasless.json";
 
 ReactDOM.render(
-  <App manifestId={sampleManifest} options={{ showTitle: false }} />,
+  <App manifestId={sampleManifest} />,
   document.getElementById("root"),
 );
