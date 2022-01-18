e52e923887dda7676e7040c8dc7b6dabb88ad283
Mat Jordan <mat@northwestern.edu>
2022-01-18 14:22:22 -0500

    Add typescript definitions for key press hook.

--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -4,9 +4,9 @@ import { useEffect } from "react";
  * @param {string} key - the name of the key to respond to, compared against event.key
  * @param {function} action - the action to perform on key press
  */
-const useKeyPress = (key, action) => {
+const useKeyPress = (key: string, action: () => void) => {
   useEffect(() => {
-    function onKeyup(e) {
+    function onKeyup(e: KeyboardEvent) {
       if (e.key === key) action();
     }
     window.addEventListener("keyup", onKeyup);
