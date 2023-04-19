67af8c4a6c311d1e1a394b3bf813bbab450ff7e8
mat <mat@northwestern.edu>
2023-04-19 15:11:09 -0400

    Add David Pugh interview.

--- src/dev/manifests.ts
+++ src/dev/manifests.ts
@@ -28,6 +28,10 @@ export const manifests = [
     label: "Basin",
   },
   {
+    url: "https://api.artic.edu/api/v1/artworks/25865/manifest.json",
+    label: "David Pugh (Interview, WebVTT Transcript)",
+  },
+  {
     url: "https://view.nls.uk/manifest/7549/75496599/manifest.json",
     label: "Edinburgh characters",
   },
