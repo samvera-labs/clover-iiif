c8313c39042a5a63fd24d22d624b87f46d26c195
Mat Jordan <mat@northwestern.edu>
2023-08-04 10:16:25 -0400

    Update `dd` rendering for individiual value entries.

--- src/components/Primitives/Markup/Markup.tsx
+++ src/components/Primitives/Markup/Markup.tsx
@@ -7,7 +7,7 @@ import { createMarkup, sanitizeAttributes } from "src/lib/html-element";
 
 import { PrimitivesMarkup } from "src/types/primitives";
 import React from "react";
-import { getLabelAsString } from "src/lib/label-helpers";
+import { getLabelEntries } from "src/lib/label-helpers";
 import { styled } from "src/styles/stitches.config";
 
 const StyledMarkup = styled("span", {});
@@ -24,12 +24,22 @@ const Markup: React.FC<PrimitivesMarkup> = (props) => {
   const remove = ["as", "markup"];
   let attributes = sanitizeAttributes(props, remove);
 
-  const html = createMarkup(
-    getLabelAsString(markup, attributes.lang as string, delimiter) as string
-  );
+  const entries = getLabelEntries(markup, attributes.lang as string);
+
+  const MarkupEntry = ({ value }: { value: string }) => {
+    const html = createMarkup(value);
+
+    return (
+      <StyledMarkup as={as} {...attributes} dangerouslySetInnerHTML={html} />
+    );
+  };
 
   return (
-    <StyledMarkup as={as} {...attributes} dangerouslySetInnerHTML={html} />
+    <>
+      {entries?.map((entry) => (
+        <MarkupEntry value={entry} />
+      ))}
+    </>
   );
 };
 
