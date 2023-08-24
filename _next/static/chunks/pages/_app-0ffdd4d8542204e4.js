140a4d91849210fa6898c7efa48bb9fadc3e237b
adamjarling <adamjarling@users.noreply.github.com>
2023-08-24 14:05:57 +0000

    Deploying to gh-pages from @ samvera-labs/clover-iiif@5dae4fe47c6d5683cb4793d0259b8e418a3ae419 🚀

---
+++ _next/static/chunks/pages/_app-0ffdd4d8542204e4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{8410:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(8913)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(8410),_(5909)}),_N_E=n.O()}]);
