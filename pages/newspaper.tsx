5ec96a1d7287c471555a84fd6535093fe4b44387
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-12-09 13:27:08 -0600

    add page to display newspaper example

---
+++ pages/newspaper.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Viewer from "docs/components/DynamicImports/Viewer";
+
+function Newspaper() {
+  const iiifContent =
+    "http://localhost:3000/manifest/newspaper/newspaper_collection.json";
+
+  return (
+    <Viewer
+      iiifContent={iiifContent}
+      options={{ informationPanel: { renderAbout: true } }}
+    />
+  );
+}
+
+export default Newspaper;
