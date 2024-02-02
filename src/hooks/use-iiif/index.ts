f6f0cdd3bd62c7bb6ec00228921dbee1f7f510e5
Adam Joseph Arling <aarling@gmail.com>
2024-02-02 15:52:47 -0600

    Remove old references to getSupplementingResources() helper function

--- src/hooks/use-iiif/index.ts
+++ src/hooks/use-iiif/index.ts
@@ -1,17 +1,15 @@
 import { getAccompanyingCanvasImage } from "src/hooks/use-iiif/getAccompanyingCanvasImage";
+import { getAnnotationResources } from "src/hooks/use-iiif/getAnnotationResources";
 import { getCanvasByCriteria } from "src/hooks/use-iiif/getCanvasByCriteria";
 import { getLabel } from "src/hooks/use-iiif/getLabel";
 import { getPaintingResource } from "src/hooks/use-iiif/getPaintingResource";
-import { getSupplementingResources } from "src/hooks/use-iiif/getSupplementingResources";
 import { getThumbnail } from "src/hooks/use-iiif/getThumbnail";
-import { getAnnotationResources } from "src/hooks/use-iiif/getAnnotationResources";
 
 export {
   getAccompanyingCanvasImage,
   getCanvasByCriteria,
   getLabel,
   getPaintingResource,
-  getSupplementingResources,
   getThumbnail,
   getAnnotationResources,
 };
