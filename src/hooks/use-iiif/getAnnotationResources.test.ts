974953b550cf695d80d83380c3f804aa83ee348c
Adam Joseph Arling <aarling@gmail.com>
2024-02-02 15:40:10 -0600

    Finish tests for getAnnotationResources helpers

--- src/hooks/use-iiif/getAnnotationResources.test.ts
+++ src/hooks/use-iiif/getAnnotationResources.test.ts
@@ -2,16 +2,14 @@ import {
   imagesAnnotations,
   multipleHighlighting,
   nonRectangularPolygon,
+  recipe0219captionFile,
   simpleAnnotations,
   simpleTagging,
 } from "src/fixtures/use-iiif/get-annotation-resources";
-import {
-  manifestNoAnnotations,
-  vttManifest,
-} from "src/fixtures/use-iiif/get-supplementing-resources";
 
 import { Vault } from "@iiif/vault";
 import { getAnnotationResources } from "./getAnnotationResources";
+import { manifestNoAnnotations } from "src/fixtures/use-iiif/get-supplementing-resources";
 
 describe("getAnnotationResources method", () => {
   it("processes manifest with simple annotations", async () => {
@@ -135,7 +133,7 @@ describe("getAnnotationResources method", () => {
     expect(result).toStrictEqual(expected);
   });
 
-  it.only("processes manifest with multiple highlighting annotations", async () => {
+  it("processes manifest with multiple highlighting annotations", async () => {
     const vault = new Vault();
     await vault.loadManifest("", multipleHighlighting);
 
@@ -198,31 +196,30 @@ describe("getAnnotationResources method", () => {
 
     const expected = [
       {
-        id: "Annotation",
+        id: "https://iiif.io/api/cookbook/recipe/0377-image-in-annotation/canvas-1/annopage-2",
+        type: "AnnotationPage",
+        behavior: [],
+        motivation: null,
+        label: {
+          none: ["Annotations"],
+        },
+        thumbnail: [],
+        summary: null,
+        requiredStatement: null,
+        metadata: [],
+        rights: null,
+        provider: [],
         items: [
           {
-            body: [
-              {
-                format: "image/jpeg",
-                id: "https://iiif.io/api/image/3.0/example/reference/918ecd18c2592080851777620de9bcb5-fountain/full/300,/0/default.jpg",
-                type: "Image",
-              },
-              {
-                id: "vault://69cc99ce",
-                language: "en",
-                type: "TextualBody",
-                value:
-                  "Night picture of the Gänseliesel fountain in Göttingen taken during the 2019 IIIF Conference",
-              },
-            ],
-            target:
-              "https://iiif.io/api/cookbook/recipe/0377-image-in-annotation/canvas-1#xywh=138,550,1477,1710",
+            id: "https://iiif.io/api/cookbook/recipe/0377-image-in-annotation/canvas-1/annopage-2/anno-1",
+            type: "Annotation",
           },
         ],
-        label: {
-          en: ["Annotation"],
-        },
-        motivation: "commenting",
+        seeAlso: [],
+        homepage: [],
+        logo: [],
+        rendering: [],
+        service: [],
       },
     ];
     expect(result).toStrictEqual(expected);
@@ -240,15 +237,44 @@ describe("getAnnotationResources method", () => {
     expect(result).toHaveLength(0);
   });
 
-  it("returns an empty array if manifest has supplementing motivation", async () => {
+  it("Processes a manifest with supplementing motivation (for example VTT)", async () => {
     const vault = new Vault();
-    await vault.loadManifest("", vttManifest);
+    await vault.loadManifest("", recipe0219captionFile);
+
+    const expected = [
+      {
+        id: "https://iiif.io/api/cookbook/recipe/0219-using-caption-file/canvas/page2",
+        type: "AnnotationPage",
+        behavior: [],
+        motivation: null,
+        label: {
+          none: ["Annotations"],
+        },
+        thumbnail: [],
+        summary: null,
+        requiredStatement: null,
+        metadata: [],
+        rights: null,
+        provider: [],
+        items: [
+          {
+            id: "https://iiif.io/api/cookbook/recipe/0219-using-caption-file/canvas/page2/a1",
+            type: "Annotation",
+          },
+        ],
+        seeAlso: [],
+        homepage: [],
+        logo: [],
+        rendering: [],
+        service: [],
+      },
+    ];
 
     const result = getAnnotationResources(
       vault,
-      "https://iiif.stack.rdc-staging.library.northwestern.edu/public/iiif3/9d/67/f8/c5/-1/a2/e-/4f/bd/-8/47/1-/4a/1a/fa/7d/6e/5a-manifest.json/canvas/ca6a621f-d3dd-43b2-8aed-40fdfda4c024",
+      "https://iiif.io/api/cookbook/recipe/0219-using-caption-file/canvas",
     );
 
-    expect(result).toHaveLength(0);
+    expect(result).toStrictEqual(expected);
   });
 });
