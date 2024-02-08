28a19398157e33b808153e2d1fc290e8652093fd
Adam Joseph Arling <aarling@gmail.com>
2024-02-08 09:46:37 -0600

    PR cleanup AnnotationItem component

--- src/components/Viewer/InformationPanel/Annotation/Item.tsx
+++ src/components/Viewer/InformationPanel/Annotation/Item.tsx
@@ -19,6 +19,8 @@ type Props = {
 };
 
 export const AnnotationItem: React.FC<Props> = ({ annotation }) => {
+  const { target } = annotation;
+
   const viewerState: ViewerContextStore = useViewerState();
   const { openSeadragonViewer, vault, activeCanvas, configOptions } =
     viewerState;
@@ -28,38 +30,38 @@ export const AnnotationItem: React.FC<Props> = ({ annotation }) => {
       label?: InternationalString;
     }
   > = annotation.body.map((body) => vault.get(body.id));
+
   const annotationBodyFormat =
     annotationBody.find((body) => body.format)?.format || "";
+
   const annotationBodyValue =
     annotationBody.find((body) => body.value)?.value || "";
 
-  function renderItemBody(annotation: AnnotationNormalized) {
-    const { target } = annotation;
+  const canvas: CanvasNormalized = vault.get({
+    id: activeCanvas,
+    type: "Canvas",
+  });
 
-    const canvas: CanvasNormalized = vault.get({
-      id: activeCanvas,
-      type: "Canvas",
-    });
+  function handleClick() {
+    if (!target) return;
 
-    function handleClick() {
-      if (!target) return;
+    const zoomLevel = configOptions.annotationOverlays?.zoomLevel || 1;
+    // @ts-ignore
+    const parsedAnnotationTarget = parseAnnotationTarget(target);
 
-      const zoomLevel = configOptions.annotationOverlays?.zoomLevel || 1;
-      // @ts-ignore
-      const parsedAnnotationTarget = parseAnnotationTarget(target);
+    const { point, rect, svg } = parsedAnnotationTarget;
 
-      const { point, rect, svg } = parsedAnnotationTarget;
-
-      if (point || rect || svg) {
-        const rect = createOpenSeadragonRect(
-          canvas,
-          parsedAnnotationTarget,
-          zoomLevel,
-        );
-        openSeadragonViewer?.viewport.fitBounds(rect);
-      }
+    if (point || rect || svg) {
+      const rect = createOpenSeadragonRect(
+        canvas,
+        parsedAnnotationTarget,
+        zoomLevel,
+      );
+      openSeadragonViewer?.viewport.fitBounds(rect);
     }
+  }
 
+  function renderItemBody() {
     switch (annotationBodyFormat) {
       case "text/plain":
         return (
@@ -82,21 +84,18 @@ export const AnnotationItem: React.FC<Props> = ({ annotation }) => {
             vttUri={annotationBody[0].id || ""}
           />
         );
+      case annotationBodyFormat.match(/^image\//)?.input:
+        const imageUri =
+          annotationBody.find((body) => !body.id?.includes("vault://"))?.id ||
+          "";
+        return (
+          <AnnotationItemImage
+            caption={annotationBodyValue}
+            handleClick={handleClick}
+            imageUri={imageUri}
+          />
+        );
       default:
-        // Is annotation body format is of type "image/*"?
-        if (/^image\//.test(annotationBodyFormat)) {
-          const imageUri =
-            annotationBody.find((body) => !body.id?.includes("vault://"))?.id ||
-            "";
-          return (
-            <AnnotationItemImage
-              caption={annotationBodyValue}
-              handleClick={handleClick}
-              imageUri={imageUri}
-            />
-          );
-        }
-
         return (
           <AnnotationItemPlainText
             value={annotationBodyValue}
@@ -106,7 +105,7 @@ export const AnnotationItem: React.FC<Props> = ({ annotation }) => {
     }
   }
 
-  return <ItemStyled>{renderItemBody(annotation)}</ItemStyled>;
+  return <ItemStyled>{renderItemBody()}</ItemStyled>;
 };
 
 export default AnnotationItem;
