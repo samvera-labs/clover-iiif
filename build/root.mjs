b2268002eaa06020f7571c706aca13f999c1cd68
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-03-08 17:41:21 -0800

    update build process to include annotation and openseadragon helpers

--- build/root.mjs
+++ build/root.mjs
@@ -3,7 +3,21 @@ import Primitives from "./primitives";
 import Scroll from "./scroll";
 import Slider from "./slider";
 import Viewer from "./viewer";
+import {
+  parseAnnotationTarget,
+  parseAnnotationsFromAnnotationResources,
+} from "./annotation-helpers";
+import { createOpenSeadragonRect } from "./openseadragon-helpers";
 
-export { Image, Primitives, Scroll, Slider, Viewer };
+export {
+  Image,
+  Primitives,
+  Scroll,
+  Slider,
+  Viewer,
+  parseAnnotationTarget,
+  parseAnnotationsFromAnnotationResources,
+  createOpenSeadragonRect
+};
 
-export default Viewer; 
+export default Viewer;
