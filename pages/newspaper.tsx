0c732b42f89aae19dbdc32d9b9ff0261cdf783e2
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-03-13 14:20:47 -0700

    update newspaer page

--- pages/newspaper.tsx
+++ pages/newspaper.tsx
@@ -9,62 +9,56 @@ const AnnotationEditor = dynamic(
   },
 );
 
-const Newspaper = () => {
-  return (
-    <EditorProvider>
-      <RenderNewspaper />
-    </EditorProvider>
-  );
-};
-
 function Demo() {
   return <div>Demo info panel</div>;
 }
 
-function RenderNewspaper() {
+function Newspaper() {
   const base_url = "http://localhost:3000";
   return (
-    <Viewer
-      iiifContent={`${base_url}/api/newspaper/collection`}
-      plugins={[
-        {
-          id: "AnnotationEditor",
-          menu: {
-            component: AnnotationEditor,
-            componentProps: {
-              annotationServer: `${base_url}/api/annotationsByCanvas/1`,
-              token: "123abc",
+    <EditorProvider>
+      <Viewer
+        iiifContent={`${base_url}/api/newspaper/collection`}
+        plugins={[
+          {
+            id: "AnnotationEditor",
+            menu: {
+              component: AnnotationEditor,
+              componentProps: {
+                annotationServer: `${base_url}/api/annotationsByCanvas/1`,
+                token: "123abc",
+              },
+            },
+            informationPanel: {
+              component: InformationPanel,
+              label: { none: ["my clip"] },
+              // displayIfNoAnnotations: false,
+              componentProps: {
+                annotationServer: `${base_url}/api/annotations/1`,
+                token: "123abc",
+              },
             },
           },
-          informationPanel: {
-            component: InformationPanel,
-            label: { none: ["my clip"] },
-            // displayIfNoAnnotations: false,
-            componentProps: {
-              annotationServer: `${base_url}/api/annotations/1`,
-              token: "123abc",
+          {
+            id: "demo",
+            informationPanel: {
+              label: { none: ["My demo"] },
+              component: Demo,
             },
           },
-        },
-        {
-          id: "demo",
-          informationPanel: {
-            label: { none: ["My demo"] },
-            component: Demo,
+        ]}
+        options={{
+          // ignoreAnnotationOverlaysLabels: ["Clippings"],
+          informationPanel: { open: true },
+          canvasHeight: "640px",
+          openSeadragon: {
+            gestureSettingsMouse: {
+              scrollToZoom: true,
+            },
           },
-        },
-      ]}
-      options={{
-        // ignoreAnnotationOverlaysLabels: ["Clippings"],
-        informationPanel: { open: true },
-        canvasHeight: "640px",
-        openSeadragon: {
-          gestureSettingsMouse: {
-            scrollToZoom: true,
-          },
-        },
-      }}
-    />
+        }}
+      />
+    </EditorProvider>
   );
 }
 
