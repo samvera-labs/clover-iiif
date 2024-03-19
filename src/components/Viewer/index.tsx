4128ea15a1a87779941fb7e48ba96e84cd5b6448
mikeapp <michael.appleby@me.com>
2024-03-18 21:13:07 -0400

    Add support in Viewer for simple Content State 1.0 constructs. (#180)

--- src/components/Viewer/index.tsx
+++ src/components/Viewer/index.tsx
@@ -14,6 +14,11 @@ import { Vault } from "@iiif/vault";
 import Viewer from "src/components/Viewer/Viewer/Viewer";
 import { createTheme } from "@stitches/react";
 import { getRequest } from "src/lib/xhr";
+import {
+  decodeContentStateContainerURI,
+  getActiveCanvas,
+  getActiveManifest,
+} from "src/lib/iiif";
 
 export interface CloverViewerProps {
   canvasIdCallback?: (arg0: string) => void;
@@ -115,7 +120,7 @@ const RenderViewer: React.FC<CloverViewerProps> = ({
           setManifest(data);
           dispatch({
             type: "updateActiveCanvas",
-            canvasId: data.items[0] && data.items[0].id,
+            canvasId: getActiveCanvas(iiifContent, data),
           });
         })
         .catch((error: Error) => {
@@ -127,16 +132,16 @@ const RenderViewer: React.FC<CloverViewerProps> = ({
             isLoaded: true,
           });
         });
-  }, [activeManifest, dispatch, vault]);
+  }, [iiifContent, activeManifest, dispatch, vault]);
 
   useEffect(() => {
     dispatch({
       type: "updateConfigOptions",
       configOptions: options,
     });
-
+    const containerURI = decodeContentStateContainerURI(iiifContent);
     vault
-      .load(iiifContent)
+      .load(containerURI)
       .then((data: CollectionNormalized | ManifestNormalized) => {
         setIiifResource(data);
       })
@@ -148,22 +153,20 @@ const RenderViewer: React.FC<CloverViewerProps> = ({
   }, [dispatch, iiifContent, options, vault]);
 
   useEffect(() => {
-    let manifests: string[] = [];
-
     if (iiifResource?.type === "Collection") {
       dispatch({
         type: "updateCollection",
         collection: iiifResource,
       });
 
-      manifests = iiifResource.items
-        .filter((item) => item.type === "Manifest")
-        .map((manifest) => manifest.id);
-
-      if (manifests.length > 0) {
+      const manifestFromContentState = getActiveManifest(
+        iiifContent,
+        iiifResource,
+      );
+      if (manifestFromContentState) {
         dispatch({
           type: "updateActiveManifest",
-          manifestId: manifests[0],
+          manifestId: manifestFromContentState,
         });
       }
     } else if (iiifResource?.type === "Manifest") {
@@ -172,7 +175,7 @@ const RenderViewer: React.FC<CloverViewerProps> = ({
         manifestId: iiifResource.id,
       });
     }
-  }, [dispatch, iiifResource]);
+  }, [dispatch, iiifContent, iiifResource]);
 
   /**
    * Render loading component while manifest is fetched and
