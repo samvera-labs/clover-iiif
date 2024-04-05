f69d99c7e3d9d5a48147051f00a86403fcb040ba
mat <mat@northwestern.edu>
2024-04-05 13:26:40 -0400

    Include image in exports.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
+import Image from "src/components/Image";
 import Primitives from "src/components/Primitives";
 import Scroll from "src/components/Scroll";
 import Slider from "src/components/Slider";
 import Viewer from "src/components/Viewer";
 
-export { Primitives, Scroll, Slider, Viewer };
+export { Image, Primitives, Scroll, Slider, Viewer };
 
 export default Viewer;
