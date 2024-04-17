d03e50f54dfcde1a4d52ac1e9adff6b23204dbe1
Trey Pendragon <tpendragon@princeton.edu>
2024-04-17 11:44:48 -0700

    Add a test for HLS loading with a parameter.

--- src/components/Viewer/Player/Player.test.tsx
+++ src/components/Viewer/Player/Player.test.tsx
@@ -9,6 +9,7 @@ import { Vault } from "@iiif/vault";
 import { ViewerProvider } from "src/context/viewer-context";
 import manifestSimpleAudio from "src/fixtures/viewer/player/manifest-simple-audio.json";
 import manifestStreaming from "src/fixtures/viewer/player/manifest-streaming-audio.json";
+import Hls from "hls.js";
 
 describe("Player component", () => {
   let originalLoad: any;
@@ -80,6 +81,69 @@ describe("Player component", () => {
 
     // Test for the audio visualizer
     expect(screen.getByRole("presentation")).toBeInTheDocument();
+  });
+
+  it("renders the Player component for a streaming audio file even if a parameter is at the end, loading hls", async () => {
+    const allSources = [
+      {
+        id: "https://meadow-streaming.rdc-staging.library.northwestern.edu/03/4a/07/03/-b/4d/3-/48/62/-b/fd/f-/85/f5/ba/8e/d1/40/034a0703-b4d3-4862-bfdf-85f5ba8ed140.m3u8?test=1",
+        type: "Sound",
+        format: "application/x-mpegurl",
+        height: 100,
+        width: 100,
+        duration: 268.776,
+      },
+    ];
+
+    const painting = {
+      id: "https://meadow-streaming.rdc-staging.library.northwestern.edu/03/4a/07/03/-b/4d/3-/48/62/-b/fd/f-/85/f5/ba/8e/d1/40/034a0703-b4d3-4862-bfdf-85f5ba8ed140.m3u8?test=1",
+      type: "Sound",
+      format: "application/x-mpegurl",
+      height: 100,
+      width: 100,
+      duration: 268.776,
+    };
+
+    const annotationResources = [];
+
+    const props = {
+      allSources: allSources as LabeledIIIFExternalWebResource[],
+      painting: painting as LabeledIIIFExternalWebResource,
+      annotationResources: annotationResources as AnnotationResources,
+    };
+
+    const hlsSpy = vi.spyOn(Hls.prototype, "attachMedia");
+
+    const vault = new Vault();
+    await vault.loadManifest("", manifestStreaming);
+
+    render(
+      <ViewerProvider
+        initialState={{
+          activeCanvas:
+            "https://dcapi.rdc-staging.library.northwestern.edu/api/v2/works/d2a423b1-6b5e-45cb-9956-46a99cd62cfd?as=iiif/canvas/access/0",
+          activeManifest:
+            "https://dcapi.rdc-staging.library.northwestern.edu/api/v2/works/d2a423b1-6b5e-45cb-9956-46a99cd62cfd?as=iiif",
+          collection: {},
+          configOptions: {},
+          customDisplays: [],
+          isInformationOpen: false,
+          isLoaded: false,
+          vault,
+          openSeadragonViewer: null,
+        }}
+      >
+        <Player {...props} />
+      </ViewerProvider>,
+    );
+
+    expect(screen.getByTestId("player-wrapper")).toBeInTheDocument();
+
+    // Test for the audio visualizer
+    expect(screen.getByRole("presentation")).toBeInTheDocument();
+
+    // Test for Hls Attach
+    expect(hlsSpy).toHaveBeenCalled();
   });
 
   it("renders the Player component for a standard audio file", async () => {
