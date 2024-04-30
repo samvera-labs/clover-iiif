9c6effbcce7568d3031f07db6bbe0644d7970dce
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-04-30 14:31:29 -0700

    fix - zoom to target when canvas changes

--- src/plugins/AnnotationEditor/components/AnnotationItem.tsx
+++ src/plugins/AnnotationEditor/components/AnnotationItem.tsx
@@ -26,11 +26,15 @@ const AnnotationItem: React.FC<PropType> = ({
   useViewerDispatch,
   setActiveTarget,
   activeTarget,
+  useViewerState,
 }) => {
   const dispatch: any = useViewerDispatch();
+  const viewerState = useViewerState();
+  const { OSDImageLoaded } = viewerState;
 
   // zoom to activeTarget when openSeadragonViewer changes
   useEffect(() => {
+    if (!OSDImageLoaded) return;
     if (!openSeadragonViewer) return;
     if (!annotation.target) return;
     if (annotation.target != activeTarget) return;
@@ -51,7 +55,7 @@ const AnnotationItem: React.FC<PropType> = ({
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [openSeadragonViewer]);
+  }, [openSeadragonViewer, OSDImageLoaded]);
 
   function handleClick() {
     if (!annotation.target) return;
@@ -73,6 +77,10 @@ const AnnotationItem: React.FC<PropType> = ({
         );
         openSeadragonViewer?.viewport.fitBounds(rect2);
       } else {
+        dispatch({
+          type: "updateOSDImageLoaded",
+          OSDImageLoaded: false,
+        });
         dispatch({
           type: "updateActiveCanvas",
           canvasId: id,
