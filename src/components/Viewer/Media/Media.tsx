2c1e71a55269eab0f5716f1b7e52c70d787aae54
Mat Jordan <mat@northwestern.edu>
2024-11-11 14:02:50 -0500

    Set default labels as index + 1 for canvas figures.

--- src/components/Viewer/Media/Media.tsx
+++ src/components/Viewer/Media/Media.tsx
@@ -95,19 +95,27 @@ const Media: React.FC<MediaProps> = ({ items }) => {
         handleFilter={handleFilter}
         handleCanvasToggle={handleCanvasToggle}
         activeIndex={activeIndex}
-        canvasLength={mediaItems.length}
+        canvasLength={items.length}
       />
       <Group
         aria-label={t("media.selectItem")}
         data-testid="media"
+        data-active-canvas={items[activeIndex].id}
+        data-canvas-length={items.length}
+        data-filter={filter}
         ref={scrollRef}
       >
         {mediaItems
-          .filter((item) => {
+          .filter((item, key) => {
+            if (!filter) return true;
+
             if (item.canvas?.label) {
               const label = getLabel(item.canvas.label);
               if (Array.isArray(label))
                 return label[0].toLowerCase().includes(filter.toLowerCase());
+            } else {
+              const label = (key + 1).toString();
+              return label.includes(filter);
             }
           })
           .map((item, index) => (
