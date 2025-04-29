d3b0362e4c2c85837ffcd25c79e08f47453a9ea0
Martin Uildriks <uildriks.m@gmail.com>
2025-04-29 15:24:59 -0400

    Removed unnecessary return statement

--- src/components/Viewer/InformationPanel/Annotation/PlainText.tsx
+++ src/components/Viewer/InformationPanel/Annotation/PlainText.tsx
@@ -9,8 +9,5 @@ type AnnotationItemPlainTextProps = {
 const AnnotationItemPlainText: React.FC<AnnotationItemPlainTextProps> = ({
   value,
   handleClick,
-}) => {
-  return <ButtonStyled onClick={handleClick}>{value}</ButtonStyled>;
-};
-
+}) => <ButtonStyled onClick={handleClick}>{value}</ButtonStyled>;
 export default AnnotationItemPlainText;
