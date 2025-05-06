850df0aaf3147476e783958cd3633e5aa7ee447b
Martin Uildriks <uildriks.m@gmail.com>
2025-05-06 07:52:12 -0400

    Added LazyLoad component to the ScrollItems in Scroll component (#273)

--- src/components/Scroll/Items/Items.tsx
+++ src/components/Scroll/Items/Items.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Reference } from "@iiif/presentation-3";
 import ScrollItem from "src/components/Scroll/Items/Item";
 import { StyledScrollItems } from "src/components/Scroll/Items/Items.styled";
+import LazyLoad from "src/components/UI/LazyLoad/LazyLoad";
 
 const ScrollItems = ({ items }: { items: Reference<"Canvas">[] }) => {
   return (
@@ -11,14 +12,16 @@ const ScrollItems = ({ items }: { items: Reference<"Canvas">[] }) => {
         const isLastItem = itemNumber === items.length;
 
         return (
-          <ScrollItem
-            item={item}
-            hasItemBreak={itemNumber < items.length}
-            isLastItem={isLastItem}
-            key={item.id}
-            itemCount={items.length}
-            itemNumber={itemNumber}
-          />
+          <LazyLoad key={index}>
+            <ScrollItem
+              item={item}
+              hasItemBreak={itemNumber < items.length}
+              isLastItem={isLastItem}
+              key={item.id}
+              itemCount={items.length}
+              itemNumber={itemNumber}
+            />
+          </LazyLoad>
         );
       })}
     </StyledScrollItems>
