c10b7d3298abfb17ced243137fd38c1ee94dcffb
Mat Jordan <mat@northwestern.edu>
2025-05-30 16:40:18 -0400

    Update inline cue handling.

--- src/components/Viewer/InformationPanel/Annotation/VTT/VTT.tsx
+++ src/components/Viewer/InformationPanel/Annotation/VTT/VTT.tsx
@@ -17,13 +17,15 @@ const AnnotationItemVTT: React.FC<AnnotationItemVTTProps> = ({
   label,
   vttUri,
 }) => {
-  const [cues, setCues] = React.useState<Array<NodeWebVttCueNested>>(inlineCues ? inlineCues : []);
+  const [cues, setCues] = React.useState<Array<NodeWebVttCueNested>>(
+    inlineCues ? inlineCues : [],
+  );
   const { createNestedCues, orderCuesByTime, parseVttData } = useWebVtt();
   const [isNetworkError, setIsNetworkError] = React.useState<Error>();
 
   useEffect(
     () => {
-      if (vttUri)
+      if (!inlineCues && vttUri) {
         fetch(vttUri, {
           headers: {
             "Content-Type": "text/plain",
@@ -42,9 +44,10 @@ const AnnotationItemVTT: React.FC<AnnotationItemVTTProps> = ({
             console.error(vttUri, error.toString());
             setIsNetworkError(error);
           });
+      }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [vttUri],
+    [vttUri, inlineCues],
   ); // NOTE: Do not include createNestedCues and orderCuesByTime in the dependency array as it will cause an infinite loop
 
   return (
