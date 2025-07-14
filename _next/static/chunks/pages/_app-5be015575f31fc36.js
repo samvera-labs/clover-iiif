dee151d06c9e2a15d1ab59d2451719cd4d9af6c7
mathewjordan <mat@northwestern.edu>
2025-07-14 15:40:06 +0000

    Deploying to gh-pages from @ samvera-labs/clover-iiif@5e01fb77bdbb797f342b64ba24a513cd5da74c1e 🚀

--- _next/static/chunks/pages/_app-5be015575f31fc36.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{1984:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(4411)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(1984),_(2323)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/pages/_error-60e80e89cdf51109.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{8330:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(1129)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=8330)}),_N_E=n.O()}]);
