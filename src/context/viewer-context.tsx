2ab4b18943df0f783a12b38370d42323c94ee165
Mark Baggett <markpbaggett@gmail.com>
2025-08-08 08:37:20 -0500

    Make cross origin be anonymous by default. (#287)

--- src/context/viewer-context.tsx
+++ src/context/viewer-context.tsx
@@ -104,7 +104,7 @@ const defaultConfigOptions = {
       zoomLevel: 4,
     },
   },
-  crossOrigin: undefined,
+  crossOrigin: "anonymous",
   ignoreCaptionLabels: [],
   informationPanel: {
     vtt: {
