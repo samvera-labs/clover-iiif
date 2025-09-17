2dbcdc011460488e1acbaa519c3a177916242880
Tarje Lavik <tarje.lavik@gmail.com>
2025-09-17 12:19:27 +0000

    Add Norwegian language support with new locale files

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,7 +1,13 @@
 import en from "./en.json";
+import nb from "./nb.json";
+import nn from "./nn.json";
+import no from "./no.json";
 import pt from "./pt.json";
 
 export default {
   en,
+  nb,
+  nn,
+  no,
   pt,
 };
