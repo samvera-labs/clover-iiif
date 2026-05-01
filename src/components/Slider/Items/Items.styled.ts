ddc826f593d92a446ce6014246849477b99ba6a2
mat <mat@northwestern.edu>
2026-04-30 21:50:58 -0500

    Replace Swiper with Embla Carousel. (#339)
    Co-authored-by: Luke Watson-Davies <83020016+lukew-cogapp@users.noreply.github.com>

--- src/components/Slider/Items/Items.styled.ts
+++ src/components/Slider/Items/Items.styled.ts
@@ -1,7 +1,16 @@
 import { styled } from "src/styles/stitches.config";
 
 const ItemsStyled = styled("div", {
-  "& .swiper-slide": {},
+  overflow: "hidden",
+  width: "100%",
+
+  "& .clover-slider-track": {
+    touchAction: "pan-y pinch-zoom",
+  },
+
+  "& .clover-slider-slide": {
+    transform: "translate3d(0, 0, 0)",
+  },
 });
 
 export { ItemsStyled };
